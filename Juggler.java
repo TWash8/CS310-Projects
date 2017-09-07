ebc0fee1e3bdd9c35e219a4fe0fa0f07f0c52051
TWash8 <thomas.washington98@gmail.com>
2017-09-06 21:16:00 -0400

    Update Juggler.java

--- Juggler.java
+++ Juggler.java
@@ -10,135 +10,135 @@ import java.util.*;
  */
 class Juggler
 {
-	/**
-	 * 
-	 */
-	private class Hand
-	{
-	     Ball[] hold = new Ball[1];
-		hold[0] = null;
-		
-	     public void catchBall (Ball ball) {
-		if (hold[0] == null) {
-		     throw (new RuntimeException);	
-		}
-		 hold[0] = ball;
-	     }
-		
-	     public Ball throwBall() {
-		if (hold[0] == null) {
-		     throw (new RuntimeException);	
-		}
-		Ball final = hold[0];
-		return hold[0] = null;
-	     }
-		
-	     public boolean hasBall() {
-	          if (hold[0] != null) {
-			return true;	  
-		  }
-		     return false;
-	     }
-		
-	     public String toString() {
-		if (!hasBall(hold)) {
-			return "   ";	
-		}
-		     return hold.toString();
-	     }
-	}
-	
-	/*-------------- DO NOT CHANGE ANYTHING BELOW THIS LINE --------------*/
-	
-	/**
-	 * DO NOT CHANGE THIS, but replace this comment
-	 */
-	private Air<Ball> air = new Air<>();
-	
-	/**
-	 * DO NOT CHANGE THIS, but replace this comment
-	 */
-	private Hand[] hands = new Hand[2];
-	
-	/**
-	 * DO NOT CHANGE THIS, but replace this comment
-	 */
-	private int numUnthrownBalls;
-	
-	/**
-	 * DO NOT CHANGE THIS, but replace this comment
-	 */
-	private int totalBalls;
-	
-	/**
-	 * DO NOT CHANGE THIS, but replace this comment
-	 */
-	public Juggler()
-	{
-		hands[0] = new Hand();
-		hands[1] = new Hand();
-		this.totalBalls = this.numUnthrownBalls = 5; //((new Random()).nextInt(5))+3;
-	}
-	
-	/**
-	 * DO NOT CHANGE THIS, but replace this comment
-	 */
-	public void passBall()
-	{
-		hands[0].catchBall(hands[1].throwBall());
-	}
-	
-	/**
-	 * DO NOT CHANGE THIS, but replace this comment
-	 */
-	public void throwBall()
-	{
-		if(!hands[0].hasBall() && this.numUnthrownBalls > 0)
-			air.add(new Ball(this.numUnthrownBalls--));
-		else
-			air.add(hands[0].throwBall());
-	}
-	
-	/**
-	 * DO NOT CHANGE THIS, but replace this comment
-	 */
-	public void catchBall()
-	{
-		hands[1].catchBall(air.remove());
-	}
-	
-	/**
-	 * DO NOT CHANGE THIS, but replace this comment
-	 * @return 
-	 */
-	public int getNumUnthrownBalls()
-	{
-		return this.numUnthrownBalls;
-	}
-	
-	/**
-	 * DO NOT CHANGE THIS, but replace this comment
-	 * @return 
-	 */
-	public String toString()
-	{
-		String spacing = "";
-		if(this.totalBalls < 7) spacing += "   ";
-		if(this.totalBalls < 5) spacing += "   ";
-		
-		return spacing + air.toString() + "\n\n      "
-			+ hands[0].toString() + "( )" + hands[1].toString() + "\n   "
-			+ "    \\__|__/\n   "
-			+ ((this.numUnthrownBalls > 6) ? "(7)" : "   ")
-			+ ((this.numUnthrownBalls > 5) ? "(6)" : "   ")
-			+ " |\n   "
-			+ ((this.numUnthrownBalls > 4) ? "(5)" : "   ")
-			+ ((this.numUnthrownBalls > 3) ? "(4)" : "   ")
-			+ " |\n   "
-			+ ((this.numUnthrownBalls > 2) ? "(3)" : "   ")
-			+ ((this.numUnthrownBalls > 1) ? "(2)" : "   ")
-			+ "/ \\\n   " 
-			+ ((this.numUnthrownBalls > 0) ? "(1)" : "   ")
-			+ "  /   \\\n";
-	}
+  /**
+   * 
+   */
+  private class Hand
+  {
+    Ball[] hold = new Ball[1];
+    
+    public void catchBall (Ball ball) {
+      if (hold[0] == null) {
+        throw new RuntimeException("no"); 
+      }
+      hold[0] = ball;
+    }
+    
+    public Ball throwBall() {
+      if (hold[0] == null) {
+        throw new RuntimeException("no"); 
+      }
+      Ball now = new Ball(hold[0].getNumber());
+      hold [0] = null;
+      return now;
+    }
+    
+    public boolean hasBall() {
+      if (hold[0] != null) {
+        return true;   
+      }
+      return false;
+    }
+    
+    public String toString() {
+      if (!hasBall()) {
+        return "   "; 
+      }
+      return hold.toString();
+    }
+  }
+  
+  /*-------------- DO NOT CHANGE ANYTHING BELOW THIS LINE --------------*/
+  
+  /**
+   * DO NOT CHANGE THIS, but replace this comment
+   */
+  private Air<Ball> air = new Air<>();
+  
+  /**
+   * DO NOT CHANGE THIS, but replace this comment
+   */
+  private Hand[] hands = new Hand[2];
+  
+  /**
+   * DO NOT CHANGE THIS, but replace this comment
+   */
+  private int numUnthrownBalls;
+  
+  /**
+   * DO NOT CHANGE THIS, but replace this comment
+   */
+  private int totalBalls;
+  
+  /**
+   * DO NOT CHANGE THIS, but replace this comment
+   */
+  public Juggler()
+  {
+    hands[0] = new Hand();
+    hands[1] = new Hand();
+    this.totalBalls = this.numUnthrownBalls = 5; //((new Random()).nextInt(5))+3;
+  }
+  
+  /**
+   * DO NOT CHANGE THIS, but replace this comment
+   */
+  public void passBall()
+  {
+    hands[0].catchBall(hands[1].throwBall());
+  }
+  
+  /**
+   * DO NOT CHANGE THIS, but replace this comment
+   */
+  public void throwBall()
+  {
+    if(!hands[0].hasBall() && this.numUnthrownBalls > 0)
+      air.add(new Ball(this.numUnthrownBalls--));
+    else
+      air.add(hands[0].throwBall());
+  }
+  
+  /**
+   * DO NOT CHANGE THIS, but replace this comment
+   */
+  public void catchBall()
+  {
+    hands[1].catchBall(air.remove());
+  }
+  
+  /**
+   * DO NOT CHANGE THIS, but replace this comment
+   * @return 
+   */
+  public int getNumUnthrownBalls()
+  {
+    return this.numUnthrownBalls;
+  }
+  
+  /**
+   * DO NOT CHANGE THIS, but replace this comment
+   * @return 
+   */
+  public String toString()
+  {
+    String spacing = "";
+    if(this.totalBalls < 7) spacing += "   ";
+    if(this.totalBalls < 5) spacing += "   ";
+    
+    return spacing + air.toString() + "\n\n      "
+      + hands[0].toString() + "( )" + hands[1].toString() + "\n   "
+      + "    \\__|__/\n   "
+      + ((this.numUnthrownBalls > 6) ? "(7)" : "   ")
+      + ((this.numUnthrownBalls > 5) ? "(6)" : "   ")
+      + " |\n   "
+      + ((this.numUnthrownBalls > 4) ? "(5)" : "   ")
+      + ((this.numUnthrownBalls > 3) ? "(4)" : "   ")
+      + " |\n   "
+      + ((this.numUnthrownBalls > 2) ? "(3)" : "   ")
+      + ((this.numUnthrownBalls > 1) ? "(2)" : "   ")
+      + "/ \\\n   " 
+      + ((this.numUnthrownBalls > 0) ? "(1)" : "   ")
+      + "  /   \\\n";
+  }
 }
