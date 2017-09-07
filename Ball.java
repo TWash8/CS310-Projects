810fc63df24f13e6ca127d14a02cb30fe7145958
TWash8 <thomas.washington98@gmail.com>
2017-09-07 10:28:59 -0400

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -11,13 +11,13 @@ class Ball
 {
   private int number = 0;
   
-  public Ball (int number) {
+  private Ball (int number) {
     this.number = number;     
   }
-  public String toString() {
+  private String toString() {
     return  ("(" + this.number + ")");     
   }
-  public int getNumber() {
+  private int getNumber() {
     return this.number;     
   }
 }
